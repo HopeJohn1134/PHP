5a27f6bb1e5df310ad08b3cb5485921bd41581c3
HopeJohn1134 <hopejohn1134@gmail.com>
2025-10-21 20:13:53 -0300

    Update ejercicio8.php

--- ejercicios/ejercicio8.php
+++ ejercicios/ejercicio8.php
@@ -8,12 +8,26 @@
     <h1>Guía de PHP - Selección de Ejercicios</h1>
     <a href="../index.php">INICIO</a>
 
-    <h3>Ejercicio 8 - Estructuras Repetitivas"</h3>
+    <h3>Ejercicio 8 - Estructuras Repetitivas</h3>
 
     <?php
-    for ($i = 0; $i <= 20; $i += 2) {
+    echo "for:<br>";
+    for ($i = 2; $i <= 20; $i += 2) {
         echo "$i<br>";
     }
+    echo "<br>while:<br>";
+    $j = 2;
+    while ($j <= 20) {
+        echo "$j<br>";
+        $j += 2;
+    }
+
+    echo "<br>do-while:<br>";
+    $k = 2;
+    do {
+        echo "$k<br>";
+        $k += 2;
+    } while ($k <= 20);
     ?>
 
 </body>
